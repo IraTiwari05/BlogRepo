b6583b130ca2e17f3e27339ab0ff0f6bd07ad6c5
Tilak2729 <tb0616964@gmail.com>
2024-03-10 14:57:03 +0530

    commit

--- src/components/accounts/Login.jsx
+++ src/components/accounts/Login.jsx
@@ -1,67 +1,73 @@
-import { useState } from 'react';
-import styled from '@emotion/styled';
-import { Box, TextField, Button, Typography} from '@mui/material'; 
-const Component=styled(Box)`
-width:400px;
-margin:auto;
-box-shadow:5px 2px 5px 2px rgb(0 0 0/0.6);
+import { useState } from "react";
+import styled from "@emotion/styled";
+import { Box, TextField, Button, Typography } from "@mui/material";
+const Component = styled(Box)`
+  width: 400px;
+  margin: auto;
+  box-shadow: 5px 2px 5px 2px rgb(0 0 0/0.6);
 `;
-const Image=styled('img')({
-   width:100,
-   margin:'auto',
-   display:'flex',
-   padding:'50px 0 0',
-})
+const Image = styled("img")({
+  width: 100,
+  margin: "auto",
+  display: "flex",
+  padding: "50px 0 0",
+});
 
-const Wrapper=styled(Box)`
-  padding:25px 35px;
-  display:flex;
-  flex:1;
-  flex-direction:column;
-  & > div,& > button,& > p{
-    margin-top:20px;
+const Wrapper = styled(Box)`
+  padding: 25px 35px;
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  & > div,
+  & > button,
+  & > p {
+    margin-top: 20px;
   }
+`;
+const LoginButton = styled(Button)`
+  text-transform: none;
+  background-color: orange;
+  color: black;
+  height: 45px;
+  text-align: center;
+  border-radius: 2px;
+`;
 
-`
-const LoginButton=styled(Button)`
-text-transform:none;    
-background-color:orange; 
-color:black;
-height:45px;
-text-align:center;
-border-radius:2px;
-`
-
- const Login = () => {
-    const imageURL = 'https://www.sesta.it/wp-content/uploads/2021/03/logo-blog-sesta-trasparente.png';
-    const[account,toggleAccount]=useState('Login');
-    const toggleSignup=()=>{
-        toggleAccount('signup');
-    }
-    return (
-      <Component>
+const Login = () => {
+  const imageURL =
+    "https://www.sesta.it/wp-content/uploads/2021/03/logo-blog-sesta-trasparente.png";
+  const [account, toggleAccount] = useState("Login");
+  const toggleSignup = () => {
+    account === "signup" ? toggleAccount("Login") : toggleAccount("signup");
+  };
+  return (
+    <Component>
       <Box>
-      <Image src={imageURL} alt="login"/>
-      { account==='Login' ?
-           <Wrapper>
-            <TextField variant="standard" label="Enter Username"/>
+        <Image src={imageURL} alt="login" />
+        {account === "Login" ? (
+          <Wrapper>
+            <TextField variant="standard" label="Enter Username" />
             <TextField variant="standard" label="Enter Password" />
             <LoginButton variant="contained">Login</LoginButton>
             <Typography>Or</Typography>
-            <Button onClick={()=>toggleSignup()} >Create an Account</Button>
-            </Wrapper>
-            :
-            <Wrapper>
-            <TextField variant="standard" label="Enter Name"/> {/*label work as place holder*/}
+            <Button onClick={() => toggleSignup()}>Create an Account</Button>
+          </Wrapper>
+        ) : (
+          <Wrapper>
+            <TextField variant="standard" label="Enter Name" />{" "}
+            {/*label work as place holder*/}
             <TextField variant="standard" label="Enter Username" />
             <TextField variant="standard" label="Enter Password" />
             <LoginButton variant="contained">Signup</LoginButton>
             <Typography>Or</Typography>
-            <Button >Already have an account</Button>
-            </Wrapper>
-       }
+            <Button onClick={() => toggleSignup()}>
+              {" "}
+              Already have an account{" "}
+            </Button>
+          </Wrapper>
+        )}
       </Box>
-      </Component>
-    )
-}
+    </Component>
+  );
+};
 export default Login;
